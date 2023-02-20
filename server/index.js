2358952591127807f5cecc54093762f64400c601
Ashutosh Rath <ashutoshrath1612@gmail.com>
2023-02-20 15:25:17 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from  "dotenv";
 import mongoose from "mongoose";
+import hotelsRoute from "./routes/hotels";
 import authRoute from "./routes/auth";
+import roomsRoute from "./routes/rooms";
+import usersRoute from "./routes/ausers;
 
 const app = express();
 dotenv.config();
@@ -21,7 +24,10 @@ mongoose.connection.on("disconnected",()=>{
 //     console.log("mongoDB connected");
 // })
 
-app.use("/auth", authRoute)
+app.use("/auth", authRoute);
+app.use("/users", usersRoute);
+app.use("/hotels", hotelsRoute);
+app.use("/rooms", roomsRoute);
 
 
 
