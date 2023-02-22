8acf91f5fa8b21ac1d973de707ceb42aa0702233
Ashutosh Rath <ashutoshrath1612@gmail.com>
2023-02-22 22:29:11 +0530

    Update auth.js

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -22,20 +22,26 @@ export const authRegister = async(req, res, next) => {
 
 };
 export const authLogin = async(req, res, next) => {
+    console.log("started")
     try{
         const user = await User.findOne({username: req.body.username})
+        console.log(user)
         if(!user)
             return next(createError(404,"User not found!"));
         else{
            const iscorrectpass = await bcrypt.compare(req.body.password,user.password);
            if(!iscorrectpass) return next(createError(400 , "Wrong password! or username"));
         }
+        try{
         const token = jwt.sign({id:user.id, isAdmin:user.isAdmin} , process.env.JWT);
 
         const {password ,isAdmin , ...others} = user._doc;
-        res.cookie("access-token",token,{
+        res.cookie("access_token",token,{
             httpOnly:true,
         }).status(200).json({...others});
+    }catch(err){
+        console.log(first)
+    }
     }
     catch(err){
         next(err);
