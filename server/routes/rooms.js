31ec312cc14ffe0943ec209ff955afba6cc92332
Ashutosh Rath <ashutoshrath1612@gmail.com>
2023-02-23 01:59:26 +0530

    Update rooms.js

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -1,5 +1,19 @@
 import express from "express";
-
+import { createRoom, deleteRoom, getallRoom, getRoom, updateRoom } from "../controller/rooms.js";
+import { verifyAdmin } from "../utils/verifytoken.js";
 const router  = express.Router();
+
+//CREATE
+router.post('/:hotelid',verifyAdmin, createRoom);
+
+//UPDATE
+router.put('/:id',verifyAdmin, updateRoom);
+//DELETE
+
+router.delete('/:id/:hotelid',verifyAdmin, deleteRoom);
+//GET
+router.get('/:id', getRoom);
+//GET ALL
+router.get('/', getallRoom);
 
 export default router
