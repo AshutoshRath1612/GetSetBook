1b7666b6bc14b1dc5026bd4e788d6d45c2169fad
Ashutosh Rath <ashutoshrath1612@gmail.com>
2023-02-24 00:16:10 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import authRoute from "./routes/auth.js";
 import roomsRoute from "./routes/rooms.js";
 import usersRoute from "./routes/users.js";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 
 const app = express();
 dotenv.config();
@@ -27,6 +27,7 @@ mongoose.connection.on("disconnected",()=>{
 // })
 
 //middlewares
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
