06831a8781fec3e4bddf26fae2e2b440923a1f03
Ashutosh Rath <ashutoshrath1612@gmail.com>
2023-02-24 00:15:00 +0530

    Update hotels.js

--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createHotel, deleteHotel, getallHotel, getHotel, updateHotel } from "../controller/hotel.js";
+import { countByCity, countByType, createHotel, deleteHotel, getallHotel, getHotel, updateHotel } from "../controller/hotel.js";
 import { verifyAdmin } from "../utils/verifytoken.js";
 const router  = express.Router();
 
@@ -15,5 +15,9 @@ router.delete('/:id',verifyAdmin, deleteHotel);
 router.get('/:id', getHotel);
 //GET ALL
 router.get('/', getallHotel);
+
+
+router.get('/find/countByCity', countByCity);
+router.get('/find/countByType', countByType);
 
 export default router
