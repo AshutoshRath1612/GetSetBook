d650ecb8d8c14909602709184bf4f35797fb2ebb
Ashutosh Rath <ashutoshrath1612@gmail.com>
2023-02-24 22:41:32 +0530

    Update Searchitem.jsx

--- client/src/Components/Searchitem.jsx
+++ client/src/Components/Searchitem.jsx
@@ -33,7 +33,7 @@ const Searchitem = ({item}) => {
             <div className="siDetailTexts">
               <span className="siPrice">₹{item.cheapestPrice}</span>
               <span className="siTaxOp">Includes taxes and fees</span>
-              <Link to={`hotel/${item._id}`}>
+              <Link to={`/hotels/${item._id}`}>
               <button className="siCheckButton">See availability</button>
               </Link>
             </div>
